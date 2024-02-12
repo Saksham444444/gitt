c6c21f37403a7b6ecbdf88c92a38defecb194d65
Saksham Humagain <saksham44humagain@gmail.com>
2024-02-12 19:56:10 +0545

    second commit

--- new.c
+++ new.c
@@ -1,6 +1,6 @@
 //Use functio  to return the sum of two numbers
 #include <stdio.h>
-int sum(int a, int b);
+int sum(int d, int b);
 int main(){
     int num_1,num_2;
     printf("Enter the first number\n");
@@ -10,7 +10,7 @@ int main(){
     printf("The sum of two numbers %d and %d is %d",num_1,num_2,sum(num_1,num_2));
     return 0;
 }
-int sum(int a,int b){
-    int c = a+b;
+int sum(int d,int b){
+    int c = d+b;
     return c;
 }
